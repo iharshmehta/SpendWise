ae55a589106393d9c6a1491df66931de99e3da9e
Harsh Mehta <hamehta20@gmail.com>
2024-02-28 02:29:06 +0530

    addedd spend description tiles in spend container with dummy data

--- src/app/spend-container/spend-container.component.ts
+++ src/app/spend-container/spend-container.component.ts
@@ -1,12 +1,48 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-spend-container',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './spend-container.component.html',
   styleUrl: './spend-container.component.scss'
 })
 export class SpendContainerComponent {
+  public spendData: any = [
+    {
+      amount: 50,
+      description: "Milk",
+      category: "grocery"
+    },
+    {
+      amount: 150,
+      description: "Chocolate",
+      category: "food"
+    },
+    {
+      amount: 500,
+      description: "Petrol",
+      category: "travel"
+    },
+    {
+      amount: 50,
+      description: "Milk",
+      category: "grocery"
+    },
+    {
+      amount: 150,
+      description: "Chocolate",
+      category: "food"
+    },
+    {
+      amount: 500,
+      description: "Petrol",
+      category: "travel"
+    }
+  ];
 
+  public getInitialOfCategory(category: string):string {
+    return category.trim().charAt(0).toUpperCase(); //get first character of category
+  }
 }
