e3fd57eac7de93df85a6957a6f8067aa7dca0788
Harsh Mehta <hamehta20@gmail.com>
2024-03-05 15:26:46 +0530

    added server task and done some naming convention changes

--- server/server.js
+++ server/server.js
@@ -15,10 +15,13 @@ const port = process.env.port || 5000;
 //json parser
 app.use(express.json());
 
+//api route for spend
 app.use("/spendwise/spend", spendRoutes);
 
+//api routes for split
 app.use("/spendwise/split", splitRoutes);
 
+//middleware for error handling
 app.use(errorHandler);
 
 app.listen(port, () => {
